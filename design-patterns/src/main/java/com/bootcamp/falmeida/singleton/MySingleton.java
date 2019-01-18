df4c1868663dbef90a7db1596c44f13333934f9f
Fabio Almeida <falmeida.tech@gmail.com>
2019-01-17 19:30:28 -0500

    Third Commit

--- design-patterns/src/main/java/com/bootcamp/falmeida/singleton/MySingleton.java
+++ design-patterns/src/main/java/com/bootcamp/falmeida/singleton/MySingleton.java
@@ -1,5 +1,16 @@
 package com.bootcamp.falmeida.singleton;
 
 public class MySingleton {
+	
+	
+	private static final MySingleton instance = new MySingleton();
+	
+	public static MySingleton getInstance() {
+		return instance;
+	}
+	
+	private MySingleton() {} // No Access
+	
+	
 
 }
